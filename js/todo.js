1d00f1c07c14a830693171cf27068260cdb39e30
KIM Jihwan <killbrain@naver.com>
2022-11-04 16:44:51 +0900

    add delete button

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+    const Li = event.target.parentElement;
+    Li.remove();
+}
+
 function paintTodo(newTOdo) {
     const Li = document.createElement("li");
     const span = document.createElement("span");
-    Li.appendChild(span);
     span.innerText = newTOdo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    Li.appendChild(span);
+    Li.appendChild(button);
     toDoList.appendChild(Li);
 }
 
