f03eda9f628312b56b91f5b73f09097aa2d15b44
KIM Jihwan <killbrain@naver.com>
2022-11-06 10:54:09 +0900

    add music

--- js/Audio.js
+++ js/Audio.js
@@ -1,9 +1,7 @@
-const cassette = document.getElementById("Cassette")
-const playButton = document.getElementById("play")
 const am = document.getElementById("AM")
 
 function togglePlay() {
     return am.paused ? am.play() : am.pause();
 };
 
-play.addEventListener("click", ogglePlay);
+play.addEventListener("click", togglePlay);
